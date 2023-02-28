0d734e42d7840a50a384f9065fc88217e96e8072
Pablo Vergara <vergarapablo2001@gmail.com>
2023-02-27 23:03:43 -0600

    Add project files.

---
+++ findroots/findroots.cpp
@@ -0,0 +1,12 @@
+﻿// findroots.cpp : Defines the entry point for the application.
+//
+
+#include "findroots.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
